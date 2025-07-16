26b647a4e92805527573482c5ce6862c1c6a8731
Muver1337 <muvers1s@gmail.com>
2025-07-16 17:19:42 +0700

    feat: добавление endpoint на просмотр Order по ID; refactor: удаление ненужного кода;

--- routes/api.php
+++ routes/api.php
@@ -7,21 +7,18 @@ use App\Http\Controllers\WarehouseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/warehouses', [WarehouseController::class, 'index']);
 
-Route::get('/warehouses', [WarehouseController::class, 'index'])->name('warehouses.index');
-
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products', [ProductController::class, 'index']);
 
 Route::prefix('orders')->name('orders.')->controller(OrderController::class)->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::post('/new', 'store')->name('store');
-    Route::patch('/{order}', 'update')->name('update');
-    Route::patch('/{order}/complete', 'complete')->name('complete');
-    Route::patch('/{order}/canceled', 'canceled')->name('canceled');
-    Route::patch('/{order}/return', 'return')->name('return');
+    Route::get('/', 'index');
+    Route::post('/new', 'store');
+    Route::get('/{order}', 'show');
+    Route::patch('/{order}', 'update');
+    Route::patch('/{order}/complete', 'complete');
+    Route::patch('/{order}/canceled', 'canceled');
+    Route::patch('/{order}/return', 'return');
 });
 
 Route::get('/movement', [StockMovementController::class, 'index']);
